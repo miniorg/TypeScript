7ecfaaa7ba023c0a1dd033415f498a386556eab6
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-07-08 13:42:47 +0900

    Add securitypolicyviolation to DocumentEventMap

--- lib/lib.dom.d.ts
+++ lib/lib.dom.d.ts
@@ -4101,6 +4101,7 @@ interface DocumentEventMap extends GlobalEventHandlersEventMap {
     "readystatechange": Event;
     "reset": Event;
     "scroll": UIEvent;
+    "securitypolicyviolation": SecurityPolicyViolationEvent;
     "seeked": Event;
     "seeking": Event;
     "select": UIEvent;
